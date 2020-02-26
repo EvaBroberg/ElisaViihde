5b30f0ee59c3ae53009332814bcbdffde3283a1a
ieva broberg <ievabroberg@gmail.com>
2020-02-26 07:59:00 +0000

    Updates

---
+++ precache-manifest.66bb9857a8db6c5ad0740db4900efe63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd090c45b9e395158fd737fea655e932",
+    "url": "/EvaBroberg/ElisaViihde/index.html"
+  },
+  {
+    "revision": "4876468559da4529e7f0",
+    "url": "/EvaBroberg/ElisaViihde/static/css/main.4c8a9a38.chunk.css"
+  },
+  {
+    "revision": "731d3bdd68c250236845",
+    "url": "/EvaBroberg/ElisaViihde/static/js/2.3e1f6554.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/EvaBroberg/ElisaViihde/static/js/2.3e1f6554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4876468559da4529e7f0",
+    "url": "/EvaBroberg/ElisaViihde/static/js/main.15b70984.chunk.js"
+  },
+  {
+    "revision": "bbd71cbfcb2268eb6668",
+    "url": "/EvaBroberg/ElisaViihde/static/js/runtime-main.400077f3.js"
+  }
+]);
